7679556428c6869ff13b255a0925dfafbeae132f
HenriqueAmrl <carlos.reis@aluno.cefet-rj.br>
2022-01-20 20:24:34 -0300

    Corrigindo lógica da questão 1

--- lab01/questao-1/script.js
+++ lab01/questao-1/script.js
@@ -1,12 +1,9 @@
 function fatorial(num) {
-    switch(num) {
-        case num < 0:
-            return "Informe um número maior ou igual a 0"
-        case num <= 1:
-            return 1
-        default:
-            return num * fatorial(num - 1)
-    }
+    if(num < 0)
+        return "Informe um número maior ou igual a 0"
+    if(num <= 1)
+        return 1
+    return num * fatorial(num - 1)
 }
     
 console.log(`Fatorial de 12 = ${fatorial(12)}`)
